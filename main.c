16b628119d13da2429acc5f0c6279eec09e73d3a
JulioSanchezD <julioe.sanchezd@gmail.com>
2020-02-10 09:40:43 -0600

    Finished alpha version, needs to fix some bugs

--- main.c
+++ main.c
@@ -3,19 +3,64 @@
 #include <string.h>
 #include "node.h"
 
+typedef enum {
+    easy = 1, medium = 3, hard = 5, ultrahard = 10
+} Level;
+Level level;
+int r_row, r_col;
+
+
+int negamax(Node *parent) {
+    char turn = (parent->turn == 'O') ? 'X' : 'O';
+    int cont, n_siblings, depth, res;
+    cont = 0;
+    depth = parent->depth + 1;
+    n_siblings = countBlankSpaces(parent->grid);
+    Node *siblings[n_siblings];
+    for (int row = 0; row < 3; row++) {
+        for (int col = 0; col < 3; col++) {
+            if (parent->grid[row][col] == ' ') {
+                siblings[cont++] = newNode(parent->grid, turn, depth, row, col);
+            }
+        }
+    }
+    int max, score;
+    max = -200;
+    Node *max_sibling;
+    for (int i = 0; i < n_siblings; i++) {
+        if (depth == level || n_siblings == 1) {
+            score = siblings[i]->score;
+        } else {
+            score = negamax(siblings[i]);
+        }
+        if (score > max) {
+            max = score;
+            max_sibling = siblings[i];
+        }
+        if (score == 100) break;
+    }
+    r_row = max_sibling->operator_coord[0];
+    r_col = max_sibling->operator_coord[1];
+    freeSiblings(siblings, n_siblings);
+    return -max;
+}
+
 int main() {
-    char grid[3][3] = {{" OX"},
-                       {" X "},
-                       {"O  "}};
-
-    Node *root = (Node *) malloc(sizeof(Node));
-    memcpy(root->grid, grid, sizeof(grid));
-    root->turn = 'X';
-    root->score = getScore(grid, root->turn);
-    root->depth = 0;
-    root->operator_coord[0] = 2;
-    root->operator_coord[1] = 0;
-
-    printNode(root);
+    setbuf(stdout, 0);
+    int row, col;
+    char grid[3][3] = {{"   "},
+                       {"   "},
+                       {"   "}};
+    level = ultrahard;
+    do {
+        printGrid(grid);
+        printf("Enter your move (row, column): ");
+        scanf("%d %d", &row, &col);
+        Node *root = newNode(grid, 'O', 0, row, col);
+        negamax(root);
+        grid[row][col] = 'O';
+        grid[r_row][r_col] = 'X';
+    } while (countBlankSpaces(grid) != 0 && getScore(grid, 'X') != abs(100));
+    printGrid(grid);
     return 0;
 }
--- node.h
+++ node.h
@@ -45,10 +45,11 @@ int getScore(char grid[3][3], char turn) {
     for (int row = 0; row < 3; row++) {
         found_x = found_o = 0;
         for (int col = 0; col < 3; col++) {
-            if (grid[row][col] == 'X') found_x = 1;
-            else if (grid[row][col] == 'O') found_o = 1;
+            if (grid[row][col] == 'X') found_x++;
+            else if (grid[row][col] == 'O') found_o++;
         }
-        if (found_x && !found_o) cost_x++;
+        if (found_x == 3 || found_o == 3) return 100;
+        else if (found_x && !found_o) cost_x++;
         else if (!found_x && found_o) cost_o++;
     }
 
@@ -56,35 +57,66 @@ int getScore(char grid[3][3], char turn) {
     for (int col = 0; col < 3; col++) {
         found_x = found_o = 0;
         for (int row = 0; row < 3; row++) {
-            if (grid[row][col] == 'X') found_x = 1;
-            else if (grid[row][col] == 'O') found_o = 1;
+            if (grid[row][col] == 'X') found_x++;
+            else if (grid[row][col] == 'O') found_o++;
         }
-        if (found_x && !found_o) cost_x++;
+        if (found_x == 3 || found_o == 3) return 100;
+        else if (found_x && !found_o) cost_x++;
         else if (!found_x && found_o) cost_o++;
     }
 
     // Check first diagonal
     found_x = found_o = 0;
     for (int i = 0; i < 3; i++) {
-        if (grid[i][i] == 'X') found_x = 1;
-        else if (grid[i][i] == 'O') found_o = 1;
+        if (grid[i][i] == 'X') found_x++;
+        else if (grid[i][i] == 'O') found_o++;
     }
-    if (found_x && !found_o) cost_x++;
+    if (found_x == 3 || found_o == 3) return 100;
+    else if (found_x && !found_o) cost_x++;
     else if (!found_x && found_o) cost_o++;
 
     // Check second diagonal
     int col = 2;
     found_x = found_o = 0;
-    for(int row = 0; row < 3; row++) {
-        if (grid[row][col] == 'X') found_x = 1;
-        else if (grid[row][col] == 'O') found_o = 1;
+    for (int row = 0; row < 3; row++) {
+        if (grid[row][col] == 'X') found_x++;
+        else if (grid[row][col] == 'O') found_o++;
         col--;
     }
+    if (found_x == 3 || found_o == 3) return 100;
     if (found_x && !found_o) cost_x++;
     else if (!found_x && found_o) cost_o++;
 
     if (turn == 'X') return cost_x - cost_o;
     else return cost_o - cost_x;
+}
+
+Node *newNode(char grid[3][3], char turn, int depth, int row_coord, int col_coord) {
+    Node *node = (Node *) malloc(sizeof(Node));
+    memcpy(node->grid, grid, sizeof(char[3][3]));
+    node->grid[row_coord][col_coord] = turn;
+    node->score = getScore(node->grid, turn);
+    node->turn = turn;
+    node->depth = depth;
+    node->operator_coord[0] = row_coord;
+    node->operator_coord[1] = col_coord;
+    return node;
+}
+
+int countBlankSpaces(char grid[3][3]) {
+    int cont = 0;
+    for (int row = 0; row < 3; row++) {
+        for (int col = 0; col < 3; col++) {
+            if (grid[row][col] == ' ') cont++;
+        }
+    }
+    return cont;
+}
+
+void freeSiblings(Node *siblings[], int n_siblings) {
+    for (int i = 0; i < n_siblings; i++) {
+        free(siblings[i]);
+    }
 }
 
 #endif //TICTACTOE_NODE_H
